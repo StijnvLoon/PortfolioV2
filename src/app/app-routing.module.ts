0197b040713c199db1f0d18439937ad0c239d561
StijnvLoon <stijnvanloon@home.nl>
2021-06-04 12:23:14 +0200

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DesktopComponent } from './desktop-components/desktop/desktop.component';
+import { MobileComponent } from './mobile-components/mobile/mobile.component';
 
-const routes: Routes = [];
+//TODO wanneer mobile?
+const mobile: boolean = window.innerWidth < 1200
+
+const mobileRoutes: Routes = [
+  { path: '', component: MobileComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+]
+
+const desktopRoutes: Routes = [
+  { path: '', component: DesktopComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(mobile ? mobileRoutes : desktopRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DesktopModule } from './desktop-components/desktop/desktop.module';
+import { MobileModule } from './mobile-components/mobile/mobile.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DesktopModule,
+    MobileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
