744959ec3d446a199302ce19523d9675df77d2ba
StijnvLoon <stijnvanloon@home.nl>
2021-08-05 21:30:07 +0200

    route transition + keywords

--- src/animations/lobbyListAnim.ts
+++ src/animations/lobbyListAnim.ts
@@ -5,7 +5,9 @@ export const lobbyListAnim =
         transition('* <=> *', [
             query(':enter',
                 [
-                    style({ opacity: 0, transform: 'translateX(-50px)' }), stagger('200ms', animate('600ms cubic-bezier(0.34, 1.56, 0.64, 1)', style({ opacity: 1, transform: 'translateX(0px)' })))
+                    style({ opacity: 0, transform: 'scale(0.8)' }),
+                    stagger('0.2s', animate('0.8s cubic-bezier(.22,1.54,.61,1)',
+                    style({ opacity: 1, transform: 'scale(1.0)' })))
                 ],
                 { optional: true }
             ),
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { routeTransitionAnim } from 'src/animations/routeTransitionAnim';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: []
+  styleUrls: [],
+  animations: [routeTransitionAnim]
 })
 export class AppComponent {
+
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
+  }
 
 }
