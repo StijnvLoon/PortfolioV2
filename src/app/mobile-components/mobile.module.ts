7c79a981a5585afd65f6ced5945e25b4c4c94321
StijnvLoon <stijnvanloon@home.nl>
2021-10-09 14:07:49 +0200

    mobile title component

--- src/app/mobile-components/mobile.module.ts
+++ src/app/mobile-components/mobile.module.ts
@@ -6,12 +6,14 @@ import { AboutComponent } from 'src/app/mobile-components/about/about.component'
 import { LobbyComponent } from 'src/app/mobile-components/lobby/lobby.component';
 import { ProjectComponent } from 'src/app/mobile-components/project/project.component';
 import { RegularModule } from '../regular.module';
+import { TitleComponent } from './project/title/title.component';
 
 @NgModule({
   declarations: [
     AboutComponent,
     LobbyComponent,
-    ProjectComponent
+    ProjectComponent,
+    TitleComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/mobile-components/project/title/title.component.ts
@@ -0,0 +1,30 @@
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { TextValue } from 'src/models/Dictionary';
+import { LanguageService } from 'src/services/language.service';
+import { SearchbarService } from 'src/services/searchbar.service';
+
+@Component({
+  selector: 'mobile-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit {
+
+  @Input() title: TextValue
+  @Input() status: TextValue
+  @Input() keywords: TextValue[]
+  @Input() scrollElement: ElementRef
+
+  constructor(
+    public languageService: LanguageService,
+    private searchbarService: SearchbarService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  searchKeyword(keyword: TextValue) {
+    this.searchbarService.setSearchText(this.languageService.get(keyword));
+  }
+
+}
