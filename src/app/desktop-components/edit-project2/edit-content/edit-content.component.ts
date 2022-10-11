a9daf17ec5f51653a6475a33e9c9aaab99e23b2f
StijnvLoon <stijnvanloon@home.nl>
2022-10-11 21:44:29 +0200

    added quill

--- src/app/desktop-components/edit-project2/edit-content/edit-content.component.ts
+++ src/app/desktop-components/edit-project2/edit-content/edit-content.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Project } from 'src/models/project/Project';
+import * as Quill from "quill";
 
 @Component({
-  selector: 'app-edit-content',
-  templateUrl: './edit-content.component.html',
-  styleUrls: ['./edit-content.component.scss']
+    selector: 'app-edit-content',
+    templateUrl: './edit-content.component.html',
+    styleUrls: ['./edit-content.component.scss']
 })
-export class EditContentComponent implements OnInit {
+export class EditContentComponent implements OnInit, OnChanges {
 
-  constructor() { }
+    @Input() project: Project
+    @Input() lang: string
 
-  ngOnInit(): void {
-  }
+    ngOnChanges(changes: SimpleChanges): void {
+        if(this.quill) {
+            const oldContent = JSON.stringify(this.quill.getContents())
+            this.project.content[changes.lang.previousValue] = oldContent
 
+            const newContent = this.project.content.get(changes.lang.currentValue)
+            this.quill.setContents(newContent ? JSON.parse(newContent) : [])
+        }
+    }
+
+    private quill
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.quill = new Quill('#editor', {
+            modules: {
+                toolbar: [
+                    [{ header: [1, 2, false] }],
+                    [{ 'size': ['small', false, 'large', 'huge'] }],
+                    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                    [{ 'script': 'sub' }, { 'script': 'super' }],
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['link', 'image', 'code-block', 'blockquote'],
+                    [{ 'color': [] }, { 'background': [] }],
+                    [{ 'align': [] }],
+                    ['clean']
+                ]
+            },
+            placeholder: 'Beschrijving van het project ...',
+            theme: 'snow'
+        })
+
+        const content = this.project.content.get(this.lang)
+        this.quill.setContents(content ? JSON.parse(content) : [])
+    }
 }
