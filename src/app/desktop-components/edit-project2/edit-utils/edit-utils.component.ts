2b4a79ab615abd4bcbeafacdc976c4c62b8fbaf9
StijnvLoon <stijnvanloon@home.nl>
2022-10-14 14:32:12 +0200

    added logos

--- src/app/desktop-components/edit-project2/edit-utils/edit-utils.component.ts
+++ src/app/desktop-components/edit-project2/edit-utils/edit-utils.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from 'src/models/project/Project';
+import { DialogService } from 'src/services/dialog.service';
+import { LanguageService } from 'src/services/language.service';
 
 @Component({
-  selector: 'app-edit-utils',
-  templateUrl: './edit-utils.component.html',
-  styleUrls: ['./edit-utils.component.scss'],
+    selector: 'app-edit-utils',
+    templateUrl: './edit-utils.component.html',
+    styleUrls: ['./edit-utils.component.scss'],
 })
 export class EditUtilsComponent implements OnInit {
 
-  constructor() { }
+    @Input() project: Project
+    @Input() lang: string
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private dialogService: DialogService,
+        public languageService: LanguageService
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    pickLogo() {
+        this.dialogService.showFilePickerDialog(
+            'file_picker_choose_image',
+            '',
+            (url: string) => {
+                if (url) this.project.logos.push(url)
+            }
+        )
+    }
+
+    removeLogo(index: number) {
+        this.project.logos.splice(index, 1)
+    }
 
 }
