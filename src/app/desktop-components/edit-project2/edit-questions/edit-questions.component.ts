94a9d47cfffbbb78a19b47f896d99533dfd929a5
StijnvLoon <stijnvanloon@home.nl>
2022-11-06 15:33:34 +0100

    added questions

--- src/app/desktop-components/edit-project2/edit-questions/edit-questions.component.ts
+++ src/app/desktop-components/edit-project2/edit-questions/edit-questions.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TextValue } from 'src/models/dict/TextValue';
+import { Project } from 'src/models/project/Project';
+import { ProjectQuestion } from 'src/models/project/Question';
+import { LanguageService } from 'src/services/language.service';
+import { ProjectService } from 'src/services/project.service';
 
 @Component({
-  selector: 'app-edit-questions',
-  templateUrl: './edit-questions.component.html',
-  styleUrls: ['./edit-questions.component.scss']
+    selector: 'app-edit-questions',
+    templateUrl: './edit-questions.component.html',
+    styleUrls: ['./edit-questions.component.scss']
 })
 export class EditQuestionsComponent implements OnInit {
 
-  constructor() { }
+    @Input() project: Project
+    @Input() lang: string
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public languageService: LanguageService,
+        private projectService: ProjectService
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    addQuestion() {
+        this.project.questions.push(new ProjectQuestion({
+            question: new TextValue({}),
+            answer: new TextValue({})
+        }))
+    }
+
+    removeQuestion(index: number) {
+        this.project.questions.splice(index, 1)
+    }
 
 }
